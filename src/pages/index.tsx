d53406b3f14480e017c36d8a707ef39845fe9813
kibo-sushant <sushant.jadhav@blueconchtech.com>
2022-11-10 13:19:48 +0530

    feat: moved CmsHomePageProducts to cms folder and deleted CmsComponent

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ import getConfig from 'next/config'
 
 import nextI18NextConfig from '../../next-i18next.config'
 import KiboHeroCarousel from '@/components/home/Carousel/KiboHeroCarousel'
-import CmsHomePageProducts from '@/components/home/CmsHomePageProducts/CmsHomePageProducts'
 import ContentTile from '@/components/home/ContentTile/ContentTile'
 import SmallBanner from '@/components/home/SmallBanner/SmallBanner'
 import { FullWidthLayout } from '@/components/layout'
